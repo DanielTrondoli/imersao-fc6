d94d4bbb9af9acf5d41c3396de22916a43d2b882
Daniel Trondoli <daniel.trondoli@monitoratec.com.br>
2022-02-14 21:20:48 -0300

    Backend in nestjs

---
+++ backend/nestjs/src/mail-list/send-mail-tweets.job.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { Process, Processor } from '@nestjs/bull';
+import { Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { Job } from 'bull';
+import { MailListService } from './mail-list.service';
+@Processor('emails')
+export class SendMailTweetsJob {
+  constructor(
+    private mailListService: MailListService,
+    @Inject('KAFKA_PRODUCER')
+    private kafkaProducer: Producer,
+    private configService: ConfigService
+    ) {}
+
+  @Process()
+  async handle(job: Job) {
+    const mailList = await this.mailListService.findOne();
+    const link = this.configService.get('NEXT_HOST');
+    await this.kafkaProducer.send({
+      topic: 'emails',
+      messages:[
+        {key: 'emails', value: JSON.stringify({
+          subject: 'Novos tweets encontrados',
+          body: `Acesse o link <a href="${link}/tweets"> Click aqui ! </a>`,
+          emails: mailList.emails,
+        })}
+      ]
+    })
+    console.log(mailList.emails);
+    console.log(' enviar mensagem o KAFKA !');
+  }
+}
